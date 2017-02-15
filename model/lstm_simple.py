2af54fe1450d551f8f99205c1ee2ba41c9bd496b
Peter <peterch1ang@yahoo.com>
2017-02-15 11:28:52 +0800

    Minor error fix

--- model/lstm_simple.py
+++ model/lstm_simple.py
@@ -3,10 +3,18 @@ import sys
 import util
 import numpy as np
 import pandas as pd
-from getopt import getopt
 
-data_dir = '../data/'
+train_data = util.load_train_data()
+train_data.drop('Date', inplace=True)
+train_data.insert(len(train_data.columns), 'DayOfWeek_1', train_data['DayOfWeek'] == 1)
+train_data.insert(len(train_data.columns), 'DayOfWeek_2', train_data['DayOfWeek'] == 2)
+train_data.insert(len(train_data.columns), 'DayOfWeek_3', train_data['DayOfWeek'] == 3)
+train_data.insert(len(train_data.columns), 'DayOfWeek_4', train_data['DayOfWeek'] == 4)
+train_data.insert(len(train_data.columns), 'DayOfWeek_5', train_data['DayOfWeek'] == 5)
+train_data.insert(len(train_data.columns), 'DayOfWeek_6', train_data['DayOfWeek'] == 6)
+train_data.insert(len(train_data.columns), 'DayOfWeek_7', train_data['DayOfWeek'] == 7)
+train_data.drop('DayOfWeek', inplace=True)
 
-train_data = util.load_train_data(data_dir)
+store_ids = train_data['Store'].unique()
 
-
+print store_ids
--- util.py
+++ util.py
@@ -2,6 +2,7 @@ import os
 import sys
 import numpy as np
 import pandas as pd
+from datetime import datetime, timedelta
 
 def strtodate(s):
     try:
