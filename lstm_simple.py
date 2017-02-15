ff18cb2cec4438789f16a42dc85cbbc990c995a6
Peter <peterch1ang@yahoo.com>
2017-02-15 11:53:42 +0800

    Restructured directory

--- lstm_simple.py
+++ lstm_simple.py
@@ -1,11 +1,11 @@
 import os
 import sys
-import util
+from src import util
 import numpy as np
 import pandas as pd
 
-train_data = util.load_train_data()
-train_data.drop('Date', inplace=True)
+train_data = util.load_splitted_data()
+del train_data['Date']
 train_data.insert(len(train_data.columns), 'DayOfWeek_1', train_data['DayOfWeek'] == 1)
 train_data.insert(len(train_data.columns), 'DayOfWeek_2', train_data['DayOfWeek'] == 2)
 train_data.insert(len(train_data.columns), 'DayOfWeek_3', train_data['DayOfWeek'] == 3)
@@ -13,7 +13,7 @@ train_data.insert(len(train_data.columns), 'DayOfWeek_4', train_data['DayOfWeek'
 train_data.insert(len(train_data.columns), 'DayOfWeek_5', train_data['DayOfWeek'] == 5)
 train_data.insert(len(train_data.columns), 'DayOfWeek_6', train_data['DayOfWeek'] == 6)
 train_data.insert(len(train_data.columns), 'DayOfWeek_7', train_data['DayOfWeek'] == 7)
-train_data.drop('DayOfWeek', inplace=True)
+del train_data['DayOfWeek']
 
 store_ids = train_data['Store'].unique()
 
