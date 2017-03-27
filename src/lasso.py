d85c15e977a11c3564ebd2a583c2688712ff8add
Zillion-Govin <zillion.govin@gmail.com>
2017-03-28 02:17:36 +0800

    Lasso and Ridge update

--- src/lasso.py
+++ src/lasso.py
@@ -32,13 +32,16 @@ def loadData(data_dir='../data/'):
 
 	data_test = np.array([d for d in data if d[2].year == 2015 and (d[2].month == 6 or d[2].month == 7)])
 
-	x_train = np.delete(data_train, [0,2,3,4,7], axis=1)
+	x_train = np.delete(data_train, [0,2,3,4,7,25], axis=1)
 	y_train = data_train[:,3]
+	std_train = np.std(data_train[:,25])
+	y_train_norm = (data_train[:,3] - data_train[:,25]) / std_train
 
-	x_test = np.delete(data_test, [0,2,3,4,7], axis=1)
-	y_test = data_test[:,3]
+	x_test = np.delete(data_test, [0,2,3,4,7,25], axis=1)
+	y_test = data_test[:, 3]
+	y_test_norm = data_test[:, [3, 25]]
 
-	return x_train, y_train, x_test, y_test
+	return x_train, y_train_norm, x_test, y_test_norm
 
 def train(x, y, model_name):
 	# load model
@@ -70,5 +73,51 @@ def test(x, y, model_name):
 
 	print('RMSPE : {}'.format(result))
 
+def test_norm(x, y, model_name):
+	with open( os.path.join('lasso/', model_name), 'rb') as infile:
+		model = pickle.load(infile)
 
+	y_predict = model.predict(x)
+	std_test = np.std(y[:,1])
+	y_predict_denorm = (y_predict * std_test) + y[:,1]
 
+	result = rmspe(y_predict_denorm, y[:,0])
+	print('RMSPE : {}'.format(result))
+
+def evaluate():
+	# TRAINING
+	data_train = util.load_train_data('../data/').values
+	x_train = np.delete(data_train, [0,2,3,4,7,25], axis=1)
+	y_train = data_train[:,3]
+	std_train = np.std(data_train[:,25])
+	mean_train = data_train[:,25]
+	y_train_norm = (data_train[:,3] - mean_train) / std_train
+
+	model = linear_model.Lasso(alpha=1.0)
+	model.fit(x_train, y_train_norm)
+
+	# TESTING
+	data_test = util.load_test_data('../data/').values
+	x_test = np.delete(data_test, [0,1,3,6,24], axis=1)
+	label = data_test[:,0]
+	mean_test = data_test[:,24]
+	std_test = np.std(data_test[:,24])
+	y_predict = model.predict(x_test)
+	y_predict_denorm = (y_predict * std_test) + mean_test
+
+	result = pd.DataFrame(np.c_[label, y_predict_denorm])
+	result.to_csv('../data/lasso_result.csv', header=['Id', 'Sales'], index=False)
+
+	return True
+
+# MAIN
+# x_train, y_train_norm, x_test, y_test_norm = loadData()
+# model = linear_model.Lasso(alpha=1.0)
+# model.fit(x_train, y_train_norm)
+
+# y_predict = model.predict(x_test)
+# std_test = np.std(y_test_norm[:,1])
+# y_predict_denorm = (y_predict * std_test) + y_test_norm[:,1]
+
+# result = rmspe(y_predict_denorm, y_test_norm[:,0])
+# print('RMSPE : {}'.format(result))
