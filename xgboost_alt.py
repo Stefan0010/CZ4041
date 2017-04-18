ef7d7f841502829ff3d744997400c1ccb6c2f5c6
Stefan0010 <dphoenix32@yahoo.com>
2017-04-18 15:46:58 +0900

    updated new results

---
+++ xgboost_alt.py
@@ -0,0 +1,253 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import xgboost as xgb
+
+import pylab
+import csv
+import datetime
+import math
+import re
+import time
+import random
+import os
+
+from pandas.tseries.offsets import *
+from operator import *
+
+from sklearn.cross_validation import train_test_split
+
+seed= 42
+
+np.set_printoptions(precision=4, threshold=10000, linewidth=100, edgeitems=999, suppress=True)
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.width', 100)
+pd.set_option('expand_frame_repr', False)
+pd.set_option('precision', 6)
+
+start_time = time.time()
+
+# Thanks to Chenglong Chen for providing this in the forum
+def ToWeight(y):
+    w = np.zeros(y.shape, dtype=float)
+    ind = y != 0
+    w[ind] = 1./(y[ind]**2)
+    return w
+
+def rmspe(yhat, y):
+    w = ToWeight(y)
+    rmspe = np.sqrt(np.mean( w * (y - yhat)**2 ))
+    return rmspe
+
+def rmspe_xg(yhat, y):
+    # y = y.values
+    y = y.get_label()
+    y = np.exp(y) - 1
+    yhat = np.exp(yhat) - 1
+    w = ToWeight(y)
+    rmspe = np.sqrt(np.mean(w * (y - yhat)**2))
+    return "rmspe", rmspe
+
+nrows = None
+
+df_train = pd.read_csv('data/train.csv', 
+                       nrows=nrows,
+                       parse_dates=['Date'],
+                       date_parser=(lambda dt: pd.to_datetime(dt, format='%Y-%m-%d')))
+
+nrows = nrows
+
+df_submit = pd.read_csv('data/test.csv', 
+                        nrows=nrows,
+                        parse_dates=['Date'],
+                        date_parser=(lambda dt: pd.to_datetime(dt, format='%Y-%m-%d')))
+
+### Setting a variable to easily distinguish train (1) from submit (0) set
+df_train['Set'] = 1
+df_submit['Set'] = 0
+
+### Combine train and test set
+frames = [df_train, df_submit]
+df = pd.concat(frames)
+
+features_x = ['Store', 'Date', 'DayOfWeek', 'Open', 'Promo', 'SchoolHoliday', 'StateHoliday']
+features_y = ['SalesLog']
+
+### Remove rows where store is open, but no sales.
+df = df.loc[~((df['Open'] == 1) & (df['Sales'] == 0))]
+
+df.loc[df['Set'] == 1, 'SalesLog'] = np.log1p(df.loc[df['Set'] == 1]['Sales']) # = np.log(df['Sales'] + 1)
+
+df['StateHoliday'] = df['StateHoliday'].astype('category').cat.codes
+
+var_name = 'Date'
+
+df[var_name + 'Day'] = pd.Index(df[var_name]).day
+df[var_name + 'Week'] = pd.Index(df[var_name]).week
+df[var_name + 'Month'] = pd.Index(df[var_name]).month
+df[var_name + 'Year'] = pd.Index(df[var_name]).year
+df[var_name + 'DayOfYear'] = pd.Index(df[var_name]).dayofyear
+
+df[var_name + 'Day'] = df[var_name + 'Day'].fillna(0)
+df[var_name + 'Week'] = df[var_name + 'Week'].fillna(0)
+df[var_name + 'Month'] = df[var_name + 'Month'].fillna(0)
+df[var_name + 'Year'] = df[var_name + 'Year'].fillna(0)
+df[var_name + 'DayOfYear'] = df[var_name + 'DayOfYear'].fillna(0)
+
+features_x.remove(var_name)
+features_x.append(var_name + 'Day')
+features_x.append(var_name + 'Week')
+features_x.append(var_name + 'Month')
+features_x.append(var_name + 'Year')
+features_x.append(var_name + 'DayOfYear')
+
+df['DateInt'] = df['Date'].astype(np.int64)
+
+df_store = pd.read_csv('data/store.csv', 
+                       nrows=nrows)
+
+### Convert Storetype and Assortment to numerical categories
+df_store['StoreType'] = df_store['StoreType'].astype('category').cat.codes
+df_store['Assortment'] = df_store['Assortment'].astype('category').cat.codes
+
+### Convert competition open year and month to float
+def convertCompetitionOpen(df):
+    try:
+        date = '{}-{}'.format(int(df['CompetitionOpenSinceYear']), int(df['CompetitionOpenSinceMonth']))
+        return pd.to_datetime(date)
+    except:
+        return np.nan
+
+df_store['CompetitionOpenInt'] = df_store.apply(lambda df: convertCompetitionOpen(df), axis=1).astype(np.int64)
+
+### Convert competition open year and month to float
+def convertPromo2(df):
+    try:
+        date = '{}{}1'.format(int(df['Promo2SinceYear']), int(df['Promo2SinceWeek']))
+        return pd.to_datetime(date, format='%Y%W%w')
+    except:
+        return np.nan
+
+df_store['Promo2SinceFloat'] = df_store.apply(lambda df: convertPromo2(df), axis=1).astype(np.int64)
+
+s = df_store['PromoInterval'].str.split(',').apply(pd.Series, 1)
+s.columns = ['PromoInterval0', 'PromoInterval1', 'PromoInterval2', 'PromoInterval3']
+df_store = df_store.join(s)
+
+def monthToNum(date):
+    return{
+            'Jan' : 1,
+            'Feb' : 2,
+            'Mar' : 3,
+            'Apr' : 4,
+            'May' : 5,
+            'Jun' : 6,
+            'Jul' : 7,
+            'Aug' : 8,
+            'Sept' : 9, 
+            'Oct' : 10,
+            'Nov' : 11,
+            'Dec' : 12
+    }[date]
+
+df_store['PromoInterval0'] = df_store['PromoInterval0'].map(lambda x: monthToNum(x) if str(x) != 'nan' else np.nan)
+df_store['PromoInterval1'] = df_store['PromoInterval1'].map(lambda x: monthToNum(x) if str(x) != 'nan' else np.nan)
+df_store['PromoInterval2'] = df_store['PromoInterval2'].map(lambda x: monthToNum(x) if str(x) != 'nan' else np.nan)
+df_store['PromoInterval3'] = df_store['PromoInterval3'].map(lambda x: monthToNum(x) if str(x) != 'nan' else np.nan)
+
+del df_store['PromoInterval']
+
+store_features = ['Store', 'StoreType', 'Assortment', 
+                  'CompetitionDistance', 'CompetitionOpenInt',
+                  'PromoInterval0']
+
+features_x = list(set(features_x + store_features))
+
+df = pd.merge(df, df_store[store_features], how='left', on=['Store'])
+
+### Convert every NAN to -1
+for feature in features_x:
+    df[feature] = df[feature].fillna(-1)
+
+def mad_based_outlier(points, thresh=3.5):
+    if len(points.shape) == 1:
+        points = points[:,None]
+    median = np.median(points, axis=0)
+    diff = np.sum((points - median)**2, axis=-1)
+    diff = np.sqrt(diff)
+    med_abs_deviation = np.median(diff)
+
+    modified_z_score = 0.6745 * diff / med_abs_deviation
+
+    return modified_z_score > thresh
+
+for i in df['Store'].unique():
+    df.loc[(df['Set'] == 1) & (df['Store'] == i) & (df['Open'] == 1), 'Outlier'] = \
+        mad_based_outlier(df.loc[(df['Set'] == 1) & (df['Store'] == i) & (df['Open'] == 1)]['Sales'], 3)
+
+print("Train a XGBoost model")
+
+### Get total sales, customers and open days per store
+store_data_sales = df.groupby([df['Store']])['Sales'].sum()
+store_data_customers = df.groupby([df['Store']])['Customers'].sum()
+store_data_open = df.groupby([df['Store']])['Open'].count()
+
+### Calculate sales per day, customers per day and sales per customers per day
+store_data_sales_per_day = store_data_sales / store_data_open
+store_data_customers_per_day = store_data_customers / store_data_open
+store_data_sales_per_customer_per_day = store_data_sales_per_day / store_data_customers_per_day
+
+df_store = pd.merge(df_store, store_data_sales_per_day.reset_index(name='SalesPerDay'), how='left', on=['Store'])
+df_store = pd.merge(df_store, store_data_customers_per_day.reset_index(name='CustomersPerDay'), how='left', on=['Store'])
+df_store = pd.merge(df_store, store_data_sales_per_customer_per_day.reset_index(name='SalesPerCustomersPerDay'), how='left', on=['Store'])
+
+store_features = ['Store', 'SalesPerDay', 'CustomersPerDay', 'SalesPerCustomersPerDay']
+
+features_x = list(set(features_x + store_features))
+
+df = pd.merge(df, df_store[store_features], how='left', on=['Store'])
+
+X_train, X_test, y_train, y_test = train_test_split(df.loc[(df['Set'] == 1) & (df['Open'] == 1)][features_x],
+                                                    df.loc[(df['Set'] == 1) & (df['Open'] == 1)][features_y],
+                                                    test_size=0.3, random_state=seed)
+
+X_train.info()
+
+dtrain = xgb.DMatrix(X_train, y_train)
+dtest = xgb.DMatrix(X_test, y_test)
+
+
+evallist = [(dtrain, 'train'), (dtest, 'test')]
+
+param = {"objective": "reg:linear",
+          "booster" : "gbtree",
+          "eta": 0.1,
+          "max_depth": 10,
+          "subsample": 0.85,
+          "colsample_bytree": 0.4,
+          "min_child_weight": 6,
+          "silent": 1,
+          "thread": 1,
+          "seed": seed
+          }
+num_round = 1200
+
+plst = param.items()
+
+bst1 = xgb.train(plst, dtrain, num_round, evallist, feval=rmspe_xg, verbose_eval=250, early_stopping_rounds=150)
+
+xgb.plot_importance(bst1)
+
+X_submit = df.loc[df['Set'] == 0]
+dsubmit = xgb.DMatrix(X_submit[features_x])
+ypred_bst = bst1.predict(dsubmit)
+df_ypred = X_submit['Id'].reset_index()
+del df_ypred['index']
+df_ypred['Id'] = df_ypred['Id'].astype('int')
+
+# Scale back the sales a bit
+df_ypred['Sales'] = (np.exp(ypred_bst) - 1) * 0.985
+df_ypred.sort_values('Id', inplace=True)
+df_ypred[['Id', 'Sales']].to_csv('rossmann_no_correction_scaled_another_Feat.csv', index=False)
